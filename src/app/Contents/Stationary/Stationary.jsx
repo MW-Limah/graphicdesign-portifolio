32feaf3edec78a86d0d14c0ff3b4b1a989ec8203
MW-Limah <cdrmw16@gmail.com>
2025-10-05 01:36:28 +0100

    adicionando conteudo ao stationary and printing design

--- src/app/Contents/Stationary/Stationary.jsx
+++ src/app/Contents/Stationary/Stationary.jsx
@@ -1,33 +1,55 @@
 import styles from "./Stationary.module.css";
+import Image from "next/image";
+import OnGoing from "../../On_going/On_going";
 
 export default function Stationary() {
   return (
     <div className={styles.container} id="stationary">
       <article className={styles.presentation}>
-        <h1>Projects: Brand Identity, Logo and Icons</h1>
+        <h1>Projects: Print Design and Stationary</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
-          ducimus doloremque alias provident et, quidem doloribus odit ipsa
-          cupiditate, dignissimos minima rerum hic vero, sed facere eius natus
-          aliquid corporis!
+          Started my career in print design, working with clients on projects
+          such as business cards, flyers, banners, and resumes. Gained hands-on
+          experience delivering high-quality printed materials tailored to
+          client needs, building a strong foundation in design and production
+          processes. Beyond design, I also managed the printing and finishing
+          stages, ensuring professional results from concept to final product.
         </p>
       </article>
       <div className={styles.projects}>
         {/* Project 1 */}
         <section className={styles.project}>
           <aside className={styles.leftside}>
-            <h2>Project 1 - EducaMap</h2>
+            <h2>Project 1 - Business card for Nail Designer</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est hic
-              numquam, facere quibusdam suscipit et dolor debitis laboriosam
-              deserunt ipsa sunt dolorem quia fugit adipisci! Repudiandae nihil
-              quis voluptates sequi?
+              This was my first project where I managed the entire process end
+              to end — from acquiring the client and understanding their needs,
+              to defining the visual style and overseeing the production of the
+              final material.
             </p>
           </aside>
           <aside className={styles.rightside}>
             <div className={styles.grid}>
-              <div className={styles.item1}></div>
-              <div className={styles.item2}></div>
+              <div className={styles.item1}>
+                <Image
+                  src={"/print-design/1.png"}
+                  fill
+                  alt="Card 1"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+              <div className={styles.item2}>
+                <Image
+                  src={"/print-design/1b.png"}
+                  fill
+                  alt="Card 1"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
             </div>
           </aside>
         </section>
@@ -35,18 +57,19 @@ export default function Stationary() {
         {/* Project 2 */}
         <section className={styles.project}>
           <aside className={styles.rightside}>
-            <div className={styles.grid}>
-              <div className={styles.item3}></div>
-              <div className={styles.item4}></div>
+            <div className={styles.grid2}>
+              <div className={styles.portrait}>
+                <Image src={"/print-design/2.png"} fill alt="Flyer" />
+              </div>
             </div>
           </aside>
           <aside className={styles.leftside}>
-            <h2>Project 2 - E-Commerce </h2>
+            <h2>Project 2 - Guitar Lessons </h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est hic
-              numquam, facere quibusdam suscipit et dolor debitis laboriosam
-              deserunt ipsa sunt dolorem quia fugit adipisci! Repudiandae nihil
-              quis voluptates sequi?
+              Personal project where I designed a flyer to promote guitar
+              lessons. The challenge was to create eye-catching visual elements
+              that conveyed the purpose of the ad even before the viewer read
+              the text.
             </p>
           </aside>
         </section>
@@ -54,20 +77,41 @@ export default function Stationary() {
         {/* Project 3 */}
         <section className={styles.project}>
           <aside className={styles.leftside}>
-            <h2>Project 3 - Party invitation</h2>
+            <h2>Project 3 - Business card for Salesperson</h2>
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
-              obcaecati aut impedit? Necessitatibus, facere. Soluta explicabo
-              tempora magnam officia id, incidunt, labore expedita, optio non
-              consequatur eius dolore dicta ducimus.
+              Project developed in close collaboration with the client, whose
+              concept guided the work except for the back design. My role was to
+              refine the idea and translate it into visuals that accurately
+              captured the client’s vision.
             </p>
           </aside>
           <aside className={styles.rightside}>
             <div className={styles.grid}>
-              <div className={styles.item1}></div>
-              <div className={styles.item2}></div>
+              <div className={styles.item1}>
+                <Image
+                  src={"/print-design/3.png"}
+                  fill
+                  alt="Card 1"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+              <div className={styles.item2}>
+                <Image
+                  src={"/print-design/3b.png"}
+                  fill
+                  alt="Card 1"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
             </div>
           </aside>
+        </section>
+        <section className={styles.project}>
+          <OnGoing />
         </section>
       </div>
     </div>
--- src/app/On_going/On_going.jsx
+++ src/app/On_going/On_going.jsx
@@ -1,11 +1,12 @@
 "use client";
+import styles from "./On_going.module.css";
 
-import { useEffect, useState } from "react";
+/* import { useEffect, useState } from "react";
 
 const frames = ["/on_going/1.png", "/on_going/2.png"];
-
+ */
 export default function FrameAnimation() {
-  const [index, setIndex] = useState(0);
+  /*   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -13,8 +14,11 @@ export default function FrameAnimation() {
     }, 400);
     return () => clearInterval(interval);
   }, []);
-
+ */
   return (
-    <img src={frames[index]} alt="animation frame" width={100} height={100} />
+    <div className={styles.ongoing}>
+      <h1>More content coming soon!</h1>
+      {/*  <img src={frames[index]} alt="animation frame" width={100} height={100} /> */}
+    </div>
   );
 }
