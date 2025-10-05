22bfab1f1fd23b881f6872108ade1bb8553986aa
MW-Limah <cdrmw16@gmail.com>
2025-10-05 23:46:37 +0100

    Adicionando media querys

--- src/app/Header/Header.jsx
+++ src/app/Header/Header.jsx
@@ -1,21 +1,24 @@
 import styles from "./Header.module.css";
-/* import Image from "next/image"; */
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <>
-      <header className={styles.header} id="home">
-        <div className={styles.asideleft}>
-          <h2>Hi, my name is</h2>
-          <h1>Williams de Lima.</h1>
-          <h3>Graphic Designer | I.T. Technician</h3>
-        </div>
-        <span className={styles.divisoria}>
-          <span className={styles.halfquad}></span>
-          <span className={styles.halfquad2}></span>
-        </span>
-        <div className={styles.asideright}></div>
-      </header>
-    </>
+    <header className={styles.header} id="home">
+      <div className={styles.asideleft}>
+        <h2>Hi, my name is</h2>
+        <h1>Williams de Lima.</h1>
+        <h3>Graphic Designer | I.T. Technician</h3>
+      </div>
+      <div className={styles.asideright}>
+        <Image
+          src="/divisoria.png"
+          alt="Divisoria"
+          width={100}
+          height={100}
+          quality={100}
+          className={styles.divisoria}
+        />
+      </div>
+    </header>
   );
 }
